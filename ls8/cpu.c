7b5617bd417146a1ec5d1b53ae5726694d06a8fe
William VanDolah <william.vandolah@gmail.com>
2018-11-28 17:12:05 -0600

    done with mvp

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "cpu.h"
 
 #define DATA_LEN 6
@@ -5,25 +8,54 @@
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  // char data[DATA_LEN] = {
+  //   // From print8.ls8
+  //   0b10000010, // LDI R0,8
+  //   0b00000000,
+  //   0b00001000,
+  //   0b01000111, // PRN R0
+  //   0b00000000,
+  //   0b00000001  // HLT
+  // };
 
+  // int address = 0;
+
+  // for (int i = 0; i < DATA_LEN; i++) {
+  //   cpu->ram[address++] = data[i];
+  // }
+  // TODO: Replace this with something less hard-coded
+  // FILE pointer
+  // char array to hold each line as a string
+  // a counter var starting at zero
+  // while loop fgets to char array
+
+  // mine
+  FILE *fp;
+  // printf("argv -> %s\n", filename);
+  char lineOf[500];
   int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  fp = fopen(filename, "r");
+  while(fgets(lineOf, sizeof(lineOf), fp)){
+    // https://www.tutorialspoint.com/c_standard_library/c_function_strtol.htm
+    char *endptr;
+    unsigned char lineToBin = strtol(lineOf, &endptr, 2);
+    // printf("what is endptr:%s", endptr);
+    // could use endptr to check if start of line is string and skip
+    // printf("does end == line: %d\n", lineOf == endptr);
+    cpu->ram[address] = lineToBin;
+    address += 1;
   }
 
-  // TODO: Replace this with something less hard-coded
+  fclose(fp);
+}
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address){
+  return cpu->ram[address];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value){
+  cpu->ram[address] = value;
 }
 
 /**
@@ -33,8 +65,10 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 {
   switch (op) {
     case ALU_MUL:
-      // TODO
+      cpu->reg[regA] *= cpu->reg[regB];
       break;
+    case ALU_ADD:
+      cpu->reg[regA] += cpu->reg[regB];
 
     // TODO: implement more ALU ops
   }
@@ -53,6 +87,48 @@ void cpu_run(struct cpu *cpu)
     // 2. switch() over it to decide on a course of action.
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
+    unsigned char IR = cpu_ram_read(cpu, cpu->PC);
+    unsigned char operandA = cpu_ram_read(cpu, cpu->PC + 1);
+    unsigned char operandB = cpu_ram_read(cpu, cpu->PC + 2);
+    unsigned char fromSP;
+    // printf("this is line| %d\n", IR);
+    switch(IR){
+      case LDI:
+        cpu->reg[operandA] = operandB;
+        break;
+      case HLT:
+        running = 0;
+        break;
+      case PRN:
+        printf("Printing in PRN: %d\n", cpu->reg[operandA]);
+        break;
+      case MUL:
+        alu(cpu, ALU_MUL, operandA, operandB);
+        break;
+      case ADD:
+        alu(cpu, ALU_ADD, operandA, operandB);
+        break;
+      case PUSH:
+        cpu->reg[SP]--;
+        cpu_ram_write(cpu, cpu->reg[SP], cpu->reg[operandA]);
+        break;
+      case POP:
+        fromSP = cpu_ram_read(cpu, cpu->reg[SP]);
+        cpu->reg[operandA] = fromSP;
+        cpu->reg[SP]++;
+        break;
+      case CALL:
+        cpu->reg[SP]--;
+        cpu_ram_write(cpu, cpu->reg[SP], cpu->PC + 1 + (IR >> 6));
+        cpu->PC = cpu->reg[operandA];
+        continue;
+      case RET:
+        fromSP = cpu_ram_read(cpu, cpu->reg[SP]);
+        cpu->PC = fromSP;
+        continue;
+
+    }
+    cpu->PC += 1 + (IR >> 6);
   }
 }
 
@@ -62,6 +138,9 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-
+  cpu->PC = 0;
+  cpu->reg[SP] = 0xF4;
   // TODO: Zero registers and RAM
+  memset(cpu->reg, 0, sizeof(cpu->reg));
+  memset(cpu->ram, 0, sizeof(cpu->ram));
 }
--- ls8/ls8.c
+++ ls8/ls8.c
@@ -4,12 +4,15 @@
 /**
  * Main
  */
-int main(void)
+int main(int argc, char **argv)
 {
   struct cpu cpu;
-
+  if(argc < 2){
+    printf("you are missing a file\n");
+    return 0;
+  }
   cpu_init(&cpu);
-  cpu_load(&cpu);
+  cpu_load(&cpu, argv[1]);
   cpu_run(&cpu);
 
   return 0;
